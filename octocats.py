88d3da21610be50bf9c347f9d75df9b7869cffc8
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-05 10:08:13 -0600

    Update octocats.py

--- octocats.py
+++ octocats.py
@@ -64,9 +64,9 @@ def newImage():
 def ScreenWipe(direction):
   if (direction == 1):
     for x in range (32):
-      temp_image = Image.new("RGB", (1, 64))
+      temp_image = Image.new("RGB", (64, 1))
       temp_draw = ImageDraw.Draw(temp_image)
-      temp_draw.line((0,x),(31,x), fill=bg_color)
+      temp_draw.rectangle((0,x,64,x), fill= bg_color)
       matrix.SetImage(temp_image, 0, x)
       sleep(20)
 
