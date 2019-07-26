ae5c84cd92c0503a81d05fffeea7e0fbe8fe45f4
jeffellenbogen <jeffellenbogen@gmail.com>
2019-07-26 21:53:04 +0100

    test commit

--- oregon.py
+++ oregon.py
@@ -10,12 +10,12 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image, ImageDraw
 
 # this is the size of ONE of our matrixes. 
-matrix_rows = 64
-matrix_columns = 64 
+matrix_rows = 32
+matrix_columns = 32 
 
 # how many matrixes stacked horizontally and vertically 
-matrix_horizontal = 1 
-matrix_vertical = 1
+matrix_horizontal = 4 
+matrix_vertical = 3
 
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
@@ -25,7 +25,8 @@ options.rows = matrix_rows
 options.cols = matrix_columns 
 options.chain_length = matrix_horizontal
 options.parallel = matrix_vertical 
-options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'adafruit-hat'
+'''options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'adafruit-hat' '''
+options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
 #options.gpio_slowdown = 2
 
 matrix = RGBMatrix(options = options)
