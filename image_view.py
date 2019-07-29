1c4ae34df3dc72693f0dee3471c79fa11a673d1a
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-29 14:50:00 -0600

    updated for scalable matrix size

--- image_view.py
+++ image_view.py
@@ -1,6 +1,8 @@
+
 # Used in main loop
 from time import sleep
 import random
+import time
 
 ###################################
 # Graphics imports, constants and structures
@@ -9,12 +11,12 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image, ImageDraw
 
 # this is the size of ONE of our matrixes. 
-matrix_rows = 64
-matrix_columns = 64 
+matrix_rows = 32 
+matrix_columns = 32 
 
 # how many matrixes stacked horizontally and vertically 
-matrix_horizontal = 1 
-matrix_vertical = 1
+matrix_horizontal = 4 
+matrix_vertical = 3 
 
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
@@ -24,19 +26,55 @@ options.rows = matrix_rows
 options.cols = matrix_columns 
 options.chain_length = matrix_horizontal
 options.parallel = matrix_vertical 
-options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'adafruit-hat'
-#options.gpio_slowdown = 2
+
+#options.hardware_mapping = 'adafruit-hat-pwm' 
+#options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.hardware_mapping = 'regular'  
+
+options.gpio_slowdown = 2
 
 matrix = RGBMatrix(options = options)
+
+imageSize = 50
+image = Image.open("./oregon/OregonO_green.jpg").convert('RGB')
+#image = image.rotate(180)
+image = image.resize((imageSize,imageSize))
+xMaxRange = total_columns - imageSize
+yMaxRange = total_rows - imageSize
+imageX= random.randint(0,xMaxRange)
+imageY= random.randint(0,yMaxRange)
+dirX = random.randint(1,3)
+dirY = random.randint(1,3)
+
+start = time.time()
+elapsed_time = 0
+last_reset = 0
+backgroundColor = (255,255,255)
 
 ###################################
 # Background
 ###################################
 def background():
-  temp_image = Image.new("RGB", (64,64))
+  global backgroundColor
+  temp_image = Image.new("RGB", (total_columns - 1,total_rows - 1))
   temp_draw = ImageDraw.Draw(temp_image)
-  temp_draw.rectangle((0,0,64,64), fill= (255,255,255))
+  temp_draw.rectangle((0,0,total_columns,total_rows), fill= backgroundColor)
   matrix.SetImage(temp_image,0,0)
+
+
+###################################
+# hideImage
+###################################
+def hideImage():
+  global image
+  global imageX
+  global imageY
+  global backgroundColor
+  temp_image = Image.new("RGB", (imageSize + 1,imageSize + 1))
+  temp_draw = ImageDraw.Draw(temp_image)
+  temp_draw.rectangle((0,0,imageSize + 1,imageSize + 1), fill= backgroundColor)
+  matrix.SetImage(temp_image,imageX, imageY)
+
 
 ###################################
 # Image Setup
@@ -56,27 +94,36 @@ else:
   image = Image.open("./logos/mustanglogo2.jpg").convert('RGB')
   
 #image = image.rotate(180)
-image = image.resize((40,40))
+image = image.resize((imageSize,imageSize))
+
 imageX = random.randint(0,24)
 imageY = random.randint(0,24)
 dirX = random.randint(1,3)
 dirY = random.randint(1,3)
 
 ###################################
+###################################
 # Main loop 
 ###################################
 background()
+#newImage()
 while True:
-  matrix.SetImage(image,imageX+dirX,imageY+dirY)
-  sleep(.25)
-  if ((imageX > 24) or (imageX < 0)):
+  if (elapsed_time > 5):
+    newImage()
+    elapsed_time=0
+    last_reset=time.time()
+    
+  elapsed_time = time.time()-last_reset    
+  matrix.SetImage(image, imageX+dirX, imageY+dirY)
+  sleep(.10)
+  if ((imageX > xMaxRange) or (imageX < 0)):
     dirX = -dirX
-  if ((imageY > 24) or (imageY < 0)):
+  if ((imageY > yMaxRange) or (imageY < 0)):
     dirY = -dirY
     
   imageX = imageX + dirX
   imageY = imageY + dirY
-  background()
+  hideImage()
 
 try:
   print("Press CTRL-C to stop")
@@ -84,4 +131,5 @@ try:
     sleep(100)
 except KeyboardInterrupt:
   exit(0)
+
 
