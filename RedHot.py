583776fab8003e7904b5d3ccf722cf81bb83aba7
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-30 14:45:54 -0600

    Update RedHot.py

--- RedHot.py
+++ RedHot.py
@@ -36,6 +36,7 @@ options.gpio_slowdown = 2
 matrix = RGBMatrix(options = options)
 
 imageSize = 80
+angle = 0
 
 #create an instance of the image object to allow for it to be used globally in functions and main loop
 image = Image.open("./logos/RedHotChiliPeppers.png").convert('RGB')
@@ -56,10 +57,16 @@ def background():
 ###################################
 # Rotate Image
 ###################################
-def rotateImage(passedDegrees):
+def rotateImage():
   # used global keyword here to access the object image in the main loop
   global image
-  image = image.rotate(passedDegrees)
+  global angle
+  image = Image.open("./logos/RedHotChiliPeppers.png").convert('RGB')
+  image = image.resize((imageSize,imageSize))
+  image = image.rotate(angle)
+  angle += 5
+  if angle > 360:
+    angle = 0
   
 
 
@@ -70,7 +77,7 @@ background()
 while True:
   matrix.SetImage(image,(total_columns - imageSize)/2,(total_rows - imageSize)/2)
   sleep(.1)
-  rotateImage(2)
+  rotateImage()
 
 try:
   print("Press CTRL-C to stop")
