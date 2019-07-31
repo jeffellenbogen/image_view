a5cd1e368f6cc6ba60d09003d2dc11a0c0485029
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-31 10:31:33 -0600

    Update dawsonLogo.py

--- dawsonLogo.py
+++ dawsonLogo.py
@@ -35,8 +35,8 @@ options.gpio_slowdown = 2
 
 matrix = RGBMatrix(options = options)
 
-imageSize = 40
-image = Image.open("./logos/mustanglogo2.jpg").convert('RGB')
+imageSize = 64
+image = Image.open("./logos/mustanglogo3.jpg").convert('RGB')
 #image = image.rotate(180)
 image = image.resize((imageSize,imageSize))
 xMaxRange = total_columns - imageSize
@@ -84,7 +84,7 @@ def hideImage():
 def newImage():
   global image
 
-  image = Image.open("./logos/mustanglogo2.jpg").convert('RGB') 
+  image = Image.open("./logos/mustanglogo3.jpg").convert('RGB') 
   image = image.resize((imageSize,imageSize))
 
 
